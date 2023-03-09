a2be4788c03506789628f4134ad1ca39ed7c142c
suraj <surajmishra9318@gmail.com>
2023-03-09 19:02:27 +0530

    Footer design Completed

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className='footer'>
+        <h5>@all rights reseverd</h5>
+      </footer>
+  )
+}
+
+export default Footer
--- src/pages/AllPost.jsx
+++ src/pages/AllPost.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from '../componets/Footer'
 import Navbar from '../componets/Navbar'
 
 const AllPost = () => {
   return (
+    <>
      <Navbar/>
+     <Footer/>
+    </>
   )
 }
 
